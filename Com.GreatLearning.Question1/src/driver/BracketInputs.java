6ddbce1b4553e9c0b1852a286f8a0976982269f9
Siddharth_M99 <siddharth.m@kuliza.com>
2022-05-02 16:34:08 +0530

    Final Commit for q1

--- Com.GreatLearning.Question1/src/driver/BracketInputs.java
+++ Com.GreatLearning.Question1/src/driver/BracketInputs.java
@@ -6,9 +6,10 @@ import java.util.Stack;
 
 public class BracketInputs {
     public static void main(String []args){
-        String brackets = "{[({)]}"; //input braces string
+        String brackets = "([[{}]]))"; //input braces string
         checkBraces ob= new checkBraces(); // for pushing or poping check method
         Stack<Character> ab = new Stack<>();
+        boolean flag = false;
         String Action;
         for(char x : brackets.toCharArray()){
             Action =  ob.checkBracesInput(x);
@@ -18,19 +19,23 @@ public class BracketInputs {
             else if(Action.equals("pop") ){
                 try {
                     if(ab.isEmpty()){
+                        flag = true;
                         System.out.println("The entered Strings do not contain Balanced Brackets");
                     }
-                    System.out.println(ab.pop());
+                    else{
+                        System.out.println(ab.pop());
+                    }
+
                 }
                 catch (Exception e){
                     e.printStackTrace();
                 }
             }
         }
-        if(ab.isEmpty()){
+        if(ab.isEmpty() && flag == false){
             System.out.println("The entered String has Balanced Brackets");
         }
-        else {
+        else if(flag == false) {
             System.out.println("The entered Strings do not contain Balanced Brackets");
         }
     }
